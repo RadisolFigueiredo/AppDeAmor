eb45c51d31884049f14b9346c85987a0b5f34aef
RadisolFigueiredo <radisolfigueiredo@gmail.com>
2021-03-06 03:15:15 -0300

    segundo splash screen

---
+++ src/screens/SplashScreenTwo/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  height: 100%;
+  align-content: center;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Box = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
