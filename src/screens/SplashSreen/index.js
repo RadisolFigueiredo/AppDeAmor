bbb9020ef21a6597eb82e0d9d736056cae114b41
RadisolFigueiredo <radisolfigueiredo@gmail.com>
2021-03-06 21:00:37 -0300

    animação splashScreen

--- src/screens/SplashSreen/index.js
+++ src/screens/SplashSreen/index.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, Animated, Text, ImageBackground} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import {Container, Box} from './styles';
+import {Container, Box, BoxTitle, Title} from './styles';
 
 export const SplashScreen = () => {
+  const backgroundImage = '../../../assets/images/backgroundCat.png';
+  const [iconPawAnimated] = useState(new Animated.Value(0));
+  const [iconUserAnimated] = useState(new Animated.Value(0));
+  const [title, setTitle] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.timing(iconPawAnimated, {
+        toValue: -100,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+      Animated.timing(iconUserAnimated, {
+        toValue: 100,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }, 1000);
+    setTimeout(() => {
+      setTitle(true);
+    }, 1500);
+  });
+
   return (
-    <Container>
-      <Box>
-        <Icon style={{paddingRight: 20}} name="paw" size={36} color="#ffff" />
-        <Icon name="user" size={36} color="#ffff" />
-      </Box>
-    </Container>
+    <ImageBackground
+      style={{width: '100%', height: '100%', resizeMode: 'cover'}}
+      source={require(backgroundImage)}>
+      <Container backgroundInitial={!title}>
+        <Box>
+          <Animated.View style={{transform: [{translateX: iconPawAnimated}]}}>
+            <Icon
+              style={{paddingRight: 20}}
+              name="paw"
+              size={36}
+              color="#ffff"
+            />
+          </Animated.View>
+          {title && (
+            <BoxTitle>
+              <Title>Dê Amor</Title>
+            </BoxTitle>
+          )}
+          <Animated.View style={{transform: [{translateX: iconUserAnimated}]}}>
+            <Icon
+              style={{paddingLeft: 20}}
+              name="user"
+              size={36}
+              color="#ffff"
+            />
+          </Animated.View>
+        </Box>
+      </Container>
+    </ImageBackground>
   );
 };
--- src/screens/SplashSreen/styles.js
+++ src/screens/SplashSreen/styles.js
@@ -1,14 +1,30 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 export const Container = styled.View`
+  width: 100%;
   height: 100%;
-  background-color: #fd9b60;
   align-content: center;
   display: flex;
   justify-content: center;
+
+  ${({backgroundInitial}) =>
+    backgroundInitial &&
+    `
+    background: #fd9b60;
+  `}
 `;
 
 export const Box = styled.View`
   flex-direction: row;
   justify-content: center;
+  height: 55px;
+  align-items: center;
+`;
+
+export const BoxTitle = styled.View`
+  position: absolute;
+`;
+export const Title = styled.Text`
+  font-size: 40px;
+  color: #ffff;
 `;
